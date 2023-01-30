871582d806467b679bade3c71105e8ef14434df9
Predrag05 <sim.pedja@gmail.com>
2023-01-30 10:47:17 +0100

    jos

--- JStutorial/theObjectType.js
+++ JStutorial/theObjectType.js
@@ -8,3 +8,23 @@ let person = {
 };
 console.log(person);
 
+let contact = {
+    firstName: "Predrag",
+    lastName: "Simonovic",
+    email: "sim.pedja@gmail.com",
+    phone: "0653595953",
+    adress: {
+        building: "2",
+        street: "Cernisevskog",
+        city: "Belgrade",
+        state: "Srbija"
+    },
+    sport: "Basketball",
+    profession: "pianist"
+};
+
+console.log(contact);
+console.log(contact.age); // undefined
+console.log(contact.adress.street); // Cernisevskog
+console.log(contact["adress"]["city"]) // може и овако али је боље са тачком, лакше
+
