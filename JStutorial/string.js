966a51457859bd2d76f5453137bcb47f60adf553
Predrag05 <sim.pedja@gmail.com>
2023-01-31 22:25:38 +0100

    tell me more

---
+++ JStutorial/string.js
@@ -0,0 +1,9 @@
+let string = "554";
+console.log(string);
+
+string = parseFloat(string); // Одлична додела - без доделе не ради
+console.log(string); // пребацио је у број
+
+let ime = "Predrag";
+ime = parseFloat(ime)
+console.log(ime) //  Даје NaN
--- JStutorial/theObjectType.js
+++ JStutorial/theObjectType.js
@@ -8,6 +8,18 @@ let person = {
 };
 console.log(person);
 
+person.age = 36; // Додавање пропертија у објекат
+console.log(person);
+
+person.jkdkjs = "ldhfgdflgd"; // Додавање пропертија у објекат
+console.log(person);
+
+delete person.jkdkjs; // Брисање пропертија у објекат
+console.log(person);
+
+console.log("persona" in person)
+console.log("age" in person)
+
 let contact = {
     firstName: "Predrag",
     lastName: "Simonovic",
@@ -23,8 +35,28 @@ let contact = {
     profession: "pianist"
 };
 
+
+
+
 console.log(contact);
 console.log(contact.age); // undefined
 console.log(contact.adress.street); // Cernisevskog
 console.log(contact["adress"]["city"]) // може и овако али је боље са тачком, лакше
 
+contact["married status"] = "Yes"; // Додавање пропертија КОЈИ ИМА РАЗМАК у објекат
+console.log(contact);
+
+contact["married status"] = "Yes Yes"; // Приступање и Измена - пропертија КОЈИ ИМА РАЗМАК
+console.log(contact);
+
+let person1 = {
+    name: 'John',
+    age: 25,
+  };
+  
+  let member = person1;
+  
+  member.age = 26;
+  
+  console.log(person1);
+  console.log(member);
