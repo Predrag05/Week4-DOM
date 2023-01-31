fdfa9c048609a16da7f5d29406dedb9ebd000b3a
Predrag05 <sim.pedja@gmail.com>
2023-01-31 23:48:06 +0100

    array friends

---
+++ JStutorial/array.js
@@ -0,0 +1,50 @@
+let friends = ["Dusan","Milan","Andrej","Zoran","Milutin","Stefan","Goran","StefanJ","Zarko","Danilo","Stevan","Ivan","Mila","JelenaR","Dragana"];
+
+friends.push("Tatjana");
+friends.push("Dragana");
+friends.push("Antonina");
+friends.push("Milena");
+friends.push("Jelena");
+
+
+friends.unshift("MihailoV");
+friends.unshift("Luiza");
+friends.unshift("Tomas");
+friends.unshift("Teodor");
+friends.unshift("Ognjen");
+friends.unshift("Teodor");
+friends.unshift("Divna");
+friends.unshift("Aleksandar");
+friends.unshift("Filip");
+friends.unshift("Sofija");
+friends.unshift("Emilija");
+friends.unshift("MihailoS");
+friends.unshift("LazarS");
+friends.unshift("Jovana");
+
+friends.push("Aleksandar");
+friends.push("Olga");
+friends.push("Jelena");
+friends.push("Dragan");
+friends.push("Katerina");
+friends.push("Slobodan");
+friends.push("Marija");
+friends.push("Ana");
+friends.push("Bojan");
+friends.push("Ivana");
+friends.push("Bogdan");
+friends.push("Stefan");
+friends.push("Marija");
+friends.push("Sava");
+friends.push("Anastasija");
+friends.push("kjgsdjkfsjd"); // овај је избрисан као последнји
+
+friends.unshift("Gospod Isus Hristos"); // unshift додаје на прво место
+
+
+
+console.log(friends)
+
+friends.pop() // брише последњег
+
+console.log(friends)
