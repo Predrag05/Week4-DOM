5673d3a3e393db2c49ae93241d845f23bb380a0c
Predrag05 <sim.pedja@gmail.com>
2023-02-01 06:42:40 +0100

    assignment operators all

---
+++ JStutorial/assignmentOperators.js
@@ -0,0 +1,71 @@
+let a = 5;
+let b = 6;
+
+
+a = b;	        // a = b	// Assigns the value of b to a.
+console.log(a,"// a = b	// Assigns the value of b to a.");
+
+a += b;	        // a = a + b	// Assigns the result of a plus b to a.
+console.log(a,"// a = a + b	// Assigns the result of a plus b to a.");
+
+a -= b;	        // a = a - b	// Assigns the result of a minus b to a.
+console.log(a,"// a = a - b	// Assigns the result of a minus b to a.");
+
+a *= b;         // a = a * b	// Assigns the result of a times b to a.
+console.log(a,"// a = a * b	// Assigns the result of a times b to a.");
+
+a /= b;	        // a = a / b	// Assigns the result of a divided by b to a.
+console.log(a,"// a = a / b	// Assigns the result of a divided by b to a.");
+
+a %= b;	        // a = a % b	// Assigns the result of a modulo b to a.
+console.log(a,"// a = a % b	// Assigns the result of a modulo b to a.");
+
+a = 5
+b = 7
+console.log(b, "бе је 7, и промењено је да би било различито опет", a,"а је 5 такође промењено")
+
+a &=b;	        // a = a & b	// Assigns the result of a AND b to a.
+console.log(a,"// a = a & b	// Assigns the result of a AND b to a.");
+
+a = 5
+b = 7
+console.log(b, "бе је 7, и промењено је да би било различито опет", a,"а је 5 такође промењено")
+
+a |=b;	        // a = a | b	// Assigns the result of a OR b to a.
+console.log(a,"// a = a | b	// Assigns the result of a OR b to a.");
+
+a = 5
+b = 7
+console.log(b, "бе је 7, и промењено је да би било различито опет", a,"а је 5 такође промењено")
+
+a ^=b;	        // a = a ^ b	// Assigns the result of a XOR b to a.
+console.log(a,"// a = a ^ b	// Assigns the result of a XOR b to a.");
+
+a = 5
+b = 7
+console.log(b, "бе је 7, и промењено је да би било различито опет", a,"а је 5 такође промењено")
+
+a <<= b;	    // a = a << b	// Assigns the result of a shifted left by b to a.
+console.log(a,"// a = a << b	// Assigns the result of a shifted left by b to a.");
+
+a = 5
+b = 7
+console.log(b, "бе је 7, и промењено је да би било различито опет", a,"а је 5 такође промењено")
+
+a >>= b	        // a = a >> b	// Assigns the result of a shifted right (sign preserved) by b to a.
+console.log(a,"// a = a >> b	// Assigns the result of a shifted right (sign preserved) by b to a.");
+
+a = 5
+b = 7
+console.log(b, "бе је 7, и промењено је да би било различито опет", a,"а је 5 такође промењено")
+
+a >>>= b        // a = a >>> b	// Assigns the result of a shifted right by b to a.
+
+
+console.log(a,"// a = a >>> b	// Assigns the result of a shifted right by b to a.");
+
+a = 5
+b = 7
+console.log(b, "бе је 7, и промењено је да би било различито опет", a,"а је 5 такође промењено")
+
+console.log("Браво Предраже за ово вежбање!!!")
