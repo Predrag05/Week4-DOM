78d1bd3dbf9b4a6a776ce598780bb09da71f8d4a
Predrag05 <sim.pedja@gmail.com>
2023-02-01 06:18:44 +0100

    idemo nizovi

--- JStutorial/array.js
+++ JStutorial/array.js
@@ -42,9 +42,63 @@ friends.push("kjgsdjkfsjd"); // овај је избрисан као после
 friends.unshift("Gospod Isus Hristos"); // unshift додаје на прво место
 
 
+console.log(friends);
 
+friends.pop(); // брише последњег
+
+console.log(friends);
+
+friends.push("Predrag");
+let indexPredrag = friends.indexOf("Predrag");
+console.log(indexPredrag);
+
+indexJovana = friends.indexOf("Jovana");
+console.log(indexJovana);
+
+indexLazar = friends.indexOf("LazarS");
+console.log(indexLazar);
+
+friends.push("DraganaDj");
+friends.push("Uros");
+friends.push("Aleksandar");
+friends.push("Branislav");
+friends.push("Veljko");
+friends.push("Luka");
+friends.push("Bozica");
+friends.push("JovanaT");
+friends.push("Sladjana");
+friends.push("Igor");
+friends.push("Biljana");
+friends.push("HristinaV");
+friends.push("Nikola");
+friends.push("Mila");
+friends.push("Petar");
+friends.push("JovanaNesic");
+friends.push("MilicaR");
+friends.push("IvanT");
+friends.push("IvanPet");
+friends.push("NatasaPet");
+friends.push("RadeTomic");
+friends.push("SladjanaTomic");
+friends.push("NenadTomic");
+friends.push("AleksandarTomic");
+friends.push("IvanaSretenovic");
+friends.push("Stojanka");
+friends.push("Vedrana");
+friends.push("Valentina");
+friends.push("Ana");
+friends.push("jklsdfaskjfhasdkjl");
+
+
+console.log(friends.length)
 console.log(friends)
+console.log(Array.isArray(friends))
 
-friends.pop() // брише последњег
+let friendsA= "Није низ :)";
 
-console.log(friends)
+if(Array.isArray(friends) === true){
+    console.log("Јесте низ, браво браво Предраже")
+}
+else{
+    console.log("пронађи грешку")
+};
