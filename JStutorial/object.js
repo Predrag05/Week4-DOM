054f2a61932107bfe2fd7767c264e39b73706a9b
Predrag05 <sim.pedja@gmail.com>
2023-02-14 23:55:53 +0100

    objekat

--- JStutorial/object.js
+++ JStutorial/object.js
@@ -1,1 +1,16 @@
+let mojObjekat = {
+    ime: 'Predrag',
+    prezime: 'Simonovic',
+    ulica: 'Brace Kovac',
+    broj: 39,
+    datum_rodjenja: '25.04.1986.'
+}
+console.log(mojObjekat.ime)
+console.log(mojObjekat.prezime)
+console.log(mojObjekat.ulica)
+console.log(mojObjekat.broj)
+console.log(mojObjekat.datum_rodjenja)
+
+
+
 console.log('Хвала ти Предраже за ово вежбање!!! Волим тее :DD')
