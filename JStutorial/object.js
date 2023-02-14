86a0411ca833365b1414f45069dbeecea95f661d
Predrag05 <sim.pedja@gmail.com>
2023-02-15 00:00:25 +0100

    objekat

--- JStutorial/object.js
+++ JStutorial/object.js
@@ -3,7 +3,8 @@ let mojObjekat = {
     prezime: 'Simonovic',
     ulica: 'Brace Kovac',
     broj: 39,
-    datum_rodjenja: '25.04.1986.'
+    datum_rodjenja: '25.04.1986.',
+    omiljeni_brojevi: [5,8,3,2,7,9,1,4,6]
 }
 console.log(mojObjekat.ime)
 console.log(mojObjekat.prezime)
@@ -11,6 +12,14 @@ console.log(mojObjekat.ulica)
 console.log(mojObjekat.broj)
 console.log(mojObjekat.datum_rodjenja)
 
+//////////////////////////////////////////
+// А може да се приступа и овако
+
+console.log(mojObjekat['ime'])
+console.log(mojObjekat['prezime'])
+console.log(mojObjekat['ulica'])
+console.log(mojObjekat['broj'])
+console.log(mojObjekat['datum_rodjenja'])
 
 
 console.log('Хвала ти Предраже за ово вежбање!!! Волим тее :DD')
