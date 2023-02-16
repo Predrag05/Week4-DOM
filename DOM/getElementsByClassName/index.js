1a46a1d4c0b89bc0ff70ef980b20cab42037a5d8
Predrag05 <sim.pedja@gmail.com>
2023-02-16 23:58:59 +0100

    idemo dalje

--- DOM/getElementsByClassName/index.js
+++ DOM/getElementsByClassName/index.js
@@ -4,5 +4,10 @@ let listaTacke = document.getElementsByClassName('item')
 let data = [].map.call(listaTacke, (tacka) => {
     return tacka.textContent;
 })
-
 console.log(data);
+
+
+let drugaLista = document.getElementsByClassName('secondary');
+let data2 = [].map.call(drugaLista, (tacka) => tacka.textContent)
+
+console.log(data2);
