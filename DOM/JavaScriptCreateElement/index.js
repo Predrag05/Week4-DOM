58a0e4d4240afaf1f126254ac1729c4c803281ac
Predrag05 <sim.pedja@gmail.com>
2023-02-18 23:05:11 +0100

    document.createElement()

---
+++ DOM/JavaScriptCreateElement/index.js
@@ -0,0 +1,20 @@
+let div = document.createElement('div');
+
+div.innerHTML = '<p>CreateElement example</p>';
+div.id = 'content';
+div.className = 'note';
+
+document.body.appendChild(div);
+
+
+let div2 = document.createElement('div');
+// create a new text node and add it to the div
+let text = document.createTextNode('CreateElement second example');
+div2.appendChild(text);
+
+document.body.appendChild(dvi2)
+
+
+
+
+console.log('ПРЕДРАЖЕ ХВАЛА ТИ ЗА ОВО ВЕЖБАЊЕ!!!');
