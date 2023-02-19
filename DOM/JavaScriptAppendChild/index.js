52683ec647b28b3ad4be3cb7150897796bc6e38f
Predrag05 <sim.pedja@gmail.com>
2023-02-19 02:00:31 +0100

    create

---
+++ DOM/JavaScriptAppendChild/index.js
@@ -0,0 +1,15 @@
+function createMenuItem(name) {
+    let li = document.createElement('li');
+    li.textContent = name;
+    return li;
+}
+// get the ul#menu
+const menu = document.querySelector('#menu');
+// add menu item
+menu.appendChild(createMenuItem('Home'));
+menu.appendChild(createMenuItem('Services'));
+menu.appendChild(createMenuItem('About Us'));
+
+
+
+console.log('ХВАЛА ТИ ПРЕДРАЖЕ ЗА БУДУЋЕ ВЕЖБАЊЕ!!!')
