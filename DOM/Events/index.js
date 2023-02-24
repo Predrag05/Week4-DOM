c4a5d27535d3cd25fad27a5534da0ed9c1a36f74
Predrag05 <sim.pedja@gmail.com>
2023-02-25 00:01:14 +0100

    event

--- DOM/Events/index.js
+++ DOM/Events/index.js
@@ -5,6 +5,8 @@ const keypress = document.getElementById('keypress');
 const focus = document.getElementById('focus');
 const blur = document.getElementById('blur');
 const resize = document.getElementById('resize');
+const focusBox = document.getElementById('focusBox');
+
 let counter = 0;
 // let nizKlik = []; komplikovanija varijanta sa ubacivanjem u niz
 
@@ -21,12 +23,12 @@ let select = document.getElementById('grad');
 
 select.addEventListener('change', () => {
     let val = document.getElementById('grad').value;
-    document.getElementById('demo').innerHTML = val
+    document.getElementById('demo').innerHTML = val;
 })
 
 
 mouseover.addEventListener('mouseover', () => {
-    mouseover.innerHTML = 'Tako je Pedja'
+    mouseover.innerHTML = 'Tako je Pedja';
 })
 
 keypress.addEventListener('keypress', () => {
@@ -37,6 +39,13 @@ keypress.addEventListener('keypress', () => {
 
 focus.addEventListener('focus', () => {
     focus.style.backgroundColor = 'yellow';
+    focusBox.style.backgroundColor = 'gold';
+})
+
+focusBox.addEventListener('keypress', () => {
+    let p = document.createElement('p');
+    p.innerHTML = focusBox.value;
+    document.body.appendChild(p);
 })
 
 blur.addEventListener('blur', () => {
